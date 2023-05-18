5d127065227b1451ddbccac3d13da4d61c2c15c6
marcobusacca <marcobusacca01@gmail.com>
2023-05-18 15:34:01 +0200

    [FEAT] dichiarazione e inizializzazione constanti

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ let numero_chilometri_utente;
 let fascia_età_utente;
 let sconto_biglietto;
 let prezzo_biglietto_totale;
+
+// DICHIARAZIONE CONSTANTI
+const prezzo_biglietto_unitario = 0.21;
+console.log(`prezzo_biglietto_unitario : ${prezzo_biglietto_unitario}€`);
+
+const sconto_minorenni = 20;
+console.log(`sconto_minorenni : ${sconto_minorenni}%`);
+
+const sconto_over_65 = 40;
+console.log(`sconto_over_65 : ${sconto_over_65}%`);
